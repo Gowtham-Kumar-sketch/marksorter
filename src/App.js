5ef6a8436d0383f0f45a95877ac613e95b482e2e
gowtham m <gowthameha1921@gmail.com>
2024-01-18 10:14:47 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { PDFViewer } from '@react-pdf/renderer';
+import ExportPDFView from './Quotation/Index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main1'>
+      <PDFViewer width='100%' height='100%'>
+        <ExportPDFView />
+      </PDFViewer>
     </div>
   );
 }
